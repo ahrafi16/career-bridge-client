80a357e06451f03cc8be6ffb0435f5c565f5bd64
ahrafi16 <anjumhossainrafi16@gmail.com>
2026-01-22 04:05:44 +0600

    Added login and register page

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,18 +1,28 @@
 import { createBrowserRouter } from "react-router";
 import RootLayout from "../layouts/RootLayout";
 import Home from "../pages/Home/Home";
+import Register from "../pages/Register/Register";
+import SignIn from "../pages/Signin/SignIn";
 
 const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <RootLayout></RootLayout>,
-    children:[
-        {
-            index: true,
-            element: <Home></Home>
-        }
-    ]
-  },
+    {
+        path: "/",
+        element: <RootLayout></RootLayout>,
+        children: [
+            {
+                index: true,
+                element: <Home></Home>
+            },
+            {
+                path: "/register",
+                element: <Register></Register>
+            },
+            {
+                path: "/signin",
+                element: <SignIn></SignIn>
+            },
+        ]
+    },
 ]);
 
 export default router;
