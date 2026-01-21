1c466fb4fc1265d23c5286093c3d4b5530785e7d
ahrafi16 <anjumhossainrafi16@gmail.com>
2026-01-22 03:30:47 +0600

    Added the header and footer

---
+++ src/router/router.jsx
@@ -0,0 +1,18 @@
+import { createBrowserRouter } from "react-router";
+import RootLayout from "../layouts/RootLayout";
+import Home from "../pages/Home/Home";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout></RootLayout>,
+    children:[
+        {
+            index: true,
+            element: <Home></Home>
+        }
+    ]
+  },
+]);
+
+export default router;
