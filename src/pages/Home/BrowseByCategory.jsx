7421ef12cbf00788365b2a7cfa57e40302f14709
ahrafi16 <anjumhossainrafi16@gmail.com>
2026-01-26 00:26:49 +0600

    Added new section

---
+++ src/pages/Home/BrowseByCategory.jsx
@@ -0,0 +1,14 @@
+
+
+const BrowseByCategory = () => {
+    return (
+        <div>
+            <div className="flex flex-col items-center gap-5">
+                <h2 className="text-3xl font-semibold">Browse by category</h2>
+                <p>Find the job that&nbsp;s perfect for you. about 800+ new jobs everyday</p>
+            </div>
+        </div>
+    );
+};
+
+export default BrowseByCategory;
