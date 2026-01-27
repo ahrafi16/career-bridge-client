bf918f3108de1b031a0924497a009a01e498575d
ahrafi16 <anjumhossainrafi16@gmail.com>
2026-01-28 01:21:29 +0600

    Added the jobs card

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import Banner from './Banner';
 import BrowseByCategory from './BrowseByCategory';
+import HotJobs from './HotJobs';
 
 const Home = () => {
+    const jobsPromise = fetch('http://localhost:3000/jobs')
+        .then(res => res.json())
     return (
         <div>
             <Banner></Banner>
             <BrowseByCategory></BrowseByCategory>
+            <Suspense fallback={<p>Loading jobs.....</p>}>
+                <HotJobs jobsPromise={jobsPromise}></HotJobs>
+            </Suspense>
         </div>
     );
 };
---
+++ src/pages/Home/HotJobs.jsx
@@ -0,0 +1,18 @@
+import { use } from "react";
+import JobCard from "../Shared/JobCard";
+
+
+const HotJobs = ({ jobsPromise }) => {
+    const jobs = use(jobsPromise);
+    return (
+        <div>
+            <div className="grid grid-cols-1 gap-6 md:grid-cols-4">
+                {jobs.map(job => (
+                    <JobCard key={job._id} job={job}></JobCard>
+                ))}
+            </div>
+        </div>
+    );
+};
+
+export default HotJobs;
